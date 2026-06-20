fb4cad1dba93e95d3e4ae27c1ebb05781cfe6db9
Nikita Choudhary <nikitaa9112@gmail.com>
2026-06-20 19:42:35 +0530

    cloudinary and multer setup

---
+++ backend/middleware/uploadMiddleware.js
@@ -0,0 +1,34 @@
+
+const multer = require("multer");
+
+const path = require("path");
+
+const storage = multer.diskStorage({
+
+  destination:(req,file,cb)=>{
+    cb(null, "uploads/");
+  },
+
+  filename:(req,file,cb) =>{
+    const uniqueName = Date.now() + path.extname(file.originalname);
+
+    cb(null, uniqueName);
+  }
+});
+
+const fileFilter = (req,file, cb) =>{
+
+  if(file.mimetype === "application/pdf"){
+    cb(null, true);
+  }else{
+
+    cb(new Error("Only PDF files are allowed"));
+  }
+};
+
+const upload = multer({
+  storage, fileFilter, 
+  limits:{fileSize: 25 * 1024 * 1024}
+});
+
+module.exports = upload;
